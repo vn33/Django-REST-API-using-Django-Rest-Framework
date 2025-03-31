90215ef9bd16019ea258c9fbc2a29ac3df56e377
vn33 <vishalpnaik666@gmail.com>
2025-03-31 17:30:35 +0530

    Session Authentication & Permissions

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,16 +3,17 @@ from . import views
 
 # api/products/
 urlpatterns = [
+    ## using mixin
+    # path('', views.product_mixin_view),
+    # path('<int:pk>/', views.product_mixin_view),
+    # path('update/<int:pk>/', views.product_mixin_view),
+    # path('delete/<int:pk>/', views.product_mixin_view),
     
-    path('', views.product_mixin_view),
-    path('<int:pk>/', views.product_mixin_view),
-    path('update/<int:pk>/', views.product_mixin_view),
-    path('delete/<int:pk>/', views.product_mixin_view),
-
-    # path('', views.product_list_create_view),
-    # path('update/<int:pk>/', views.product_update_view),
-    # path('delete/<int:pk>/', views.product_destroy_view),
-    # path('<int:pk>/', views.product_detail_view),
+    ## using generic
+    path('', views.product_list_create_view),
+    path('update/<int:pk>/', views.product_update_view),
+    path('delete/<int:pk>/', views.product_destroy_view),
+    path('<int:pk>/', views.product_detail_view),
     
     # path('', views.product_alt_view),
     # path('<int:pk>/', views.product_alt_view)
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, mixins
+from rest_framework import authentication, generics, mixins, permissions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
@@ -8,6 +8,8 @@ from .serializers import ProductSerializer
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         # Retrieve title and content from the validated data
